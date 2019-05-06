cf498a7df13d83a1c81e15f8b7191e93a6a7d831
Svetlana <bystrova72@mail.ru>
2019-05-06 21:34:19 +0300

    правки по комментариям

--- index.php
+++ index.php
@@ -14,79 +14,55 @@ mysqli_set_charset($link, 'utf8');
 
 $projects = [];
 $tasks = [];
-$id_name = '';
-
-//получаем ссылку
-$params = $_GET;
-$params['id_name'] = '';
-/*$scriptname = pathinfo(__FILE__, PATHINFO_BASENAME);
-$query = http_build_query($params);
-$url = '/' . $scriptname . '?' . $query;*/
+$id_user = '';
 
 
 // проверка подключения
 
 if ($link === false) {
     die ('Ошибка подключения:' . mysqli_connect_error());
-} else {
+} 
         
-    // запрос на получение списка проектов для пользователя id = 2
-    $sql_projects = "SELECT title AS category
-    FROM projects"; 
-    /*WHERE projects.id_user = 2";*/
+    $id_user = 2;
+    $projectId = null;
 
-    // запрос на получение списка задач для пользователя id = 2
+if (isset($_GET['id_name']) && $_GET['id_name'] === '') {
+    http_response_code(404);
+    require_once '404.php';
+    exit();
+}
 
-    $sql_tasks = "SELECT name AS purpose, deadline AS execution_date, status AS done, title AS category, file
+if (isset($_GET['id_name']) && $_GET['id_name'] !== '') {
+    $projectId = (int)$_GET['id_name']; // В $_GET всегда содержатся строки, а нам нужно число
+}
+
+$sql_tasks = 'SELECT name AS purpose, deadline AS execution_date, status AS done, title AS category, file
     FROM tasks 
     JOIN projects
-    ON projects.id_name = tasks.id_name";
-    /*WHERE projects.id_user = 2";*/
+    ON projects.id_name = tasks.id_name
+    WHERE tasks.id_user = ?';
 
-    //получение ресурса результата
+$sql_projects = 'SELECT  title AS category
+    FROM projects
+    JOIN tasks
+    ON projects.id_name = tasks.id_name
+    WHERE tasks.id_user = ?';
 
-    $res_projects = mysqli_query($link, $sql_projects);
+$values = [$id_user];
 
-    $res_tasks = mysqli_query($link, $sql_tasks);
-
-    //проверка запросов
-
-    if ($res_projects === false && $res_tasks === false) {
-        die ('Ошибка при выполнении SQL запроса: ' . mysqli_error($link));
-    }
-        
-    $projects = mysqli_fetch_all($res_projects, MYSQLI_ASSOC);
-    
-    $tasks = mysqli_fetch_all($res_tasks, MYSQLI_ASSOC);
-
+if ($projectId !== null) {
+    $sql_tasks .= ' AND tasks.id_name = ?';
+    $values[]  = $projectId;
 }
+$projects = db_fetch_data($link, $sql_projects, $values);
 
-if (isset($_GET['id_name'])) {
+$tasks = db_fetch_data($link, $sql_tasks, $values);
 
-if($_GET(['id_name'] === '')) {
-    @require_once($_SERVER['DOCUMENT_ROOT'].'/404.php');
+if (empty($tasks)) {
+    require_once '404.php';
     exit();
-    }
-    
-$id_name = $_GET['id_name'];
-$id_user = 2;
-
-$sql_id_name = $sql_tasks . 'WHERE tasks.id_name = ? AND tasks.id_user = ?';
-
-$stmt = db_get_prepare_stmt($link, $sql_id_name, [$id_name, $id_user]);
-mysqli_stmt_execute($stmt);
-$result = mysqli_stmt_get_result($stmt);
-
-if($result === false) {
-    exit('Ошибка при попытке получить результат prepared statement: ' . mysqli_stmt_error($stmt));}
-        
-    $tasks = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-    if (empty($tasks)) {
-        @require_once($_SERVER['DOCUMENT_ROOT'].'/404.php');
-        exit();
-    }
 }
+
 
 
 $contentOfPage = include_template ('index.php', ['tasks' => $tasks, 'show_complete_tasks' => $show_complete_tasks
@@ -95,11 +71,8 @@ $contentLayout = include_template ('layout.php', [
     'contentOfPage' => $contentOfPage, 
     'projects' => $projects,
     'tasks' => $tasks,
-    'active_project' => $id_name,
     'userName' => 'Светлана Быстрова',
     'pageName' => 'Дела в Порядке']);
 print($contentLayout);
-
-
 
 ?>
